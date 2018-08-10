a8ae095a398c33b5f17ab902254558c7fc06e96f
Ben Guilfoyle <benjamin.guilfoyle.2017@mumail.ie>
2018-08-10 11:22:04 +0100

    small edits

--- buildGrid.js
+++ buildGrid.js
@@ -1,10 +1,17 @@
 function buildGrid(size){//this function generates a grid of size relative to the game
-  for(var i = 1; i<= size; i++){
-    var newBox = document.createElement("div");
-    newBox.id = i;
-    newBox.class = "square";
-    newBox.class = "grid-item";
-    newBox.onclick = makeMove(this.id);
-    document.getElementById("gameSpace").add(newBox);
-  }
+  var table = document.createElement("table");
+  for (var i = 1; i < size; i++) {
+    var tr = document.createElement('tr');
+    for (var j = 1; j < size; j++) {
+        var td = document.createElement('td');
+        if (i%2 == j%2) {
+            td.className = "white";
+        } else {
+            td.className = "black";
+        }
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+}
+document.body.appendChild(table);
 }

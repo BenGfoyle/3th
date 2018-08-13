216cd36bc9f2eb429db880b7beac1127c7c433e9
BenGfoyle <benjamin.guilfoyle.2017@mumail.ie>
2018-08-13 11:45:57 +0100

    Grids are now generated based on the game selected

--- buildGrid.js
+++ buildGrid.js
@@ -1,10 +1,14 @@
 function buildGrid(size){//this function generates a grid of size relative to the game
   var table = document.createElement("table");
-  for (var i = 1; i < size; i++) {
+  table.className = "tableOutside";
+  for (var i = 1; i <= size; i++) {
     var tr = document.createElement('tr');
-    for (var j = 1; j < size; j++) {
+    for (var j = 1; j <= size; j++) {
         var td = document.createElement('td');
-        td.className = "white";
+        td.className = "tableInside";
+        td.id = i;
+        td.value = "";
+        td.onClick = "makeMove(td.id)";
         tr.appendChild(td);
     }
     table.appendChild(tr);
